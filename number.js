c77e33a6380b17f646326742584f8b5dc3ae5d73
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-02 20:16:18 +0300

    Create number.js

---
+++ number.js
@@ -0,0 +1,7 @@
+function addone(input){
+    let number = Number(input.shift());
+    console.log (number + 1);
+}
+addone (["19"])
+
+ako ne se sloji Number shte stane 191, a ne 20. Trqbwa da se izpolzvat stringove (w kavichki)
