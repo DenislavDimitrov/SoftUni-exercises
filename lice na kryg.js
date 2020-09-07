3de57348b1b811ffc9f6cc4f069bb1266dda5e56
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-08 00:07:57 +0300

    Create lice na kryg.js

---
+++ lice na kryg.js
@@ -0,0 +1,8 @@
+function circle (input){
+let radius = Number(input.shift());
+let area = radius * radius * Math.PI;
+console.log (area.toFixed(2));
+}
+circle (["20"])
+
+s kvadratnite skobi e masiv kato tetradka s prazni redcheta
