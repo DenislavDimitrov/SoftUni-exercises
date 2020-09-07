37bf3e3b79e9173a8a34514c718103ab6bbc01fd
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-08 00:07:55 +0300

    Create input.js

---
+++ input.js
@@ -0,0 +1,8 @@
+function greeting (input) {
+    let name = input.shift();
+    console.log (`Hello, ${name}! Your age is: 20. Your grade is: 6`);
+
+}
+greeting (["Pesho"])
+
+vzima otdolu vseki shift i go maha
