b663038c721acacf974c935d611c5f25bf5426e0
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-08 00:06:07 +0300

    Create greeting with `.js

---
+++ greeting with `.js
@@ -0,0 +1,11 @@
+function greeting (input){
+    let firstName = input.shift();
+    let lastName = input.shift();
+    let age = Number(input.shift());
+    let town = input.shift();
+
+
+    console.log (`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+
+}
+greeting (["Maria", "Ivanova", "20", "Sofia"]);
