5f9b30569f5a1d75026b00fd13253c86aec000f7
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-08 00:05:45 +0300

    Create architect.js

---
+++ architect.js
@@ -0,0 +1,7 @@
+function architect (input){
+    let name = input.shift();
+    let count = Number(input.shift());
+
+    console.log (`The architect ${name} will need ${count*3} hours to complete ${count} project/s.`)
+}
+architect (["George", "4"]);
