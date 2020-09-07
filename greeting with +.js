2aed28a690569f41c7d0781b340e76485399d3e8
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-08 00:07:48 +0300

    Create greeting with +.js

---
+++ greeting with +.js
@@ -0,0 +1,7 @@
+function greeting (name, age, grade){
+    console.log ("Hello, " + name + "! Your age is: " + age + ". Your grade is: " + grade);
+
+}
+greeting ("Gosho", 20, 6.00);
+
+konkatenaciq
