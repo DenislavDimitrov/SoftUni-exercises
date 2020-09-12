1b4e168680146151fc7faaf0de04eed73c1eb6a0
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-12 14:27:27 +0300

    Create variables2.js

---
+++ variables2.js
@@ -0,0 +1,12 @@
+function greeting (name, age, grade){
+    console.log (name);
+    console.log (age);
+    console.log (grade);
+}
+let name = "Stamat";
+let age = 16;
+let grade = 5.00;
+
+greeting (name, age, grade);
+
+може да се сложи на мястото в greetings и да се махнат данните от лет :)
