b9abe08ade0966622c6d5807e945d307c0471ea6
DenislavDimitrov <70429056+DenislavDimitrov@users.noreply.github.com>
2020-09-27 18:25:07 +0300

    Create shibanSwitch.js

---
+++ shibanSwitch.js
@@ -0,0 +1,47 @@
+function solve(day, age) {
+  let price = 0;
+
+  if (age >= 0 && age <=18) {
+    switch(day){
+        case "Weekday":
+        console.log(price+12+'$')    
+        ; break;
+        case "Weekend": 
+        console.log(price+15+'$')    
+        ; break;
+        case "Holiday":
+          console.log(price+5+'$')    
+        ; break;
+    }
+  }
+else if (age > 18 && age <=64){
+  switch(day){
+    case "Weekday":
+      console.log(price+18+'$')    
+    ; break;
+    case "Weekend": 
+    console.log(price+20+'$')  
+    ; break;
+    case "Holiday":
+      console.log(price+12+'$')     
+    ; break;
+}
+}
+else if(age > 64 && age <=122){
+  switch(day){
+    case "Weekday":
+      console.log(price+12+'$')     
+    ; break;
+    case "Weekend": 
+    console.log(price+15+'$')  
+    ; break;
+    case "Holiday":
+      console.log(price+10+'$')     
+    ; break;
+}
+}
+else {console.log('Error!')}
+}
+
+
+solve("Weekday", 100)
